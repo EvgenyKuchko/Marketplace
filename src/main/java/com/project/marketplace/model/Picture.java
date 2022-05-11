401b27612d6aab4dfa67a77031c635f6de518106
EvgenyKuchko <kuchkozhenia@gmail.com>
2022-05-11 13:08:04 +0300

    create picture class

---
+++ src/main/java/com/project/marketplace/model/Picture.java
@@ -0,0 +1,23 @@
+package com.project.marketplace.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Picture {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String filePath;
+    private float price;
+    private String creator;
+}
