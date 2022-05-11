31d0873c733f42958c70d790dea6f8233aaab2e5
EvgenyKuchko <kuchkozhenia@gmail.com>
2022-05-11 13:17:45 +0300

    create OneToMany relationship between user and picture

--- src/main/java/com/project/marketplace/model/Picture.java
+++ src/main/java/com/project/marketplace/model/Picture.java
@@ -4,10 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -20,4 +17,7 @@ public class Picture {
     private String filePath;
     private float price;
     private String creator;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
--- src/main/java/com/project/marketplace/model/User.java
+++ src/main/java/com/project/marketplace/model/User.java
@@ -1,16 +1,16 @@
 package com.project.marketplace.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,4 +18,6 @@ public class User {
     private String email;
     private String password;
     private String nickname;
+    @OneToMany
+    private Set<Picture> pictures;
 }
