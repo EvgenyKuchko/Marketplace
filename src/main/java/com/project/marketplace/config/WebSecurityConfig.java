11769406090e85cbc81d993ca8c98f0494affff2
EvgenyKuchko <kuchkozhenia@gmail.com>
2022-05-11 18:35:48 +0300

    moved WebSecurityConfig to config package

--- src/main/java/com/project/marketplace/config/WebSecurityConfig.java
+++ src/main/java/com/project/marketplace/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.project.marketplace;
+package com.project.marketplace.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
