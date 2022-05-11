9cefaeee2df57dab4a3a77100fbd0c79a428d7d3
EvgenyKuchko <kuchkozhenia@gmail.com>
2022-05-11 15:41:29 +0300

    add description fields to models

--- src/main/java/com/project/marketplace/model/User.java
+++ src/main/java/com/project/marketplace/model/User.java
@@ -18,6 +18,7 @@ public class User {
     private String email;
     private String password;
     private String nickname;
+    private String userDescription;
     @OneToMany
     private Set<Picture> pictures;
 }
