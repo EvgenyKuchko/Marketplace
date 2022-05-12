9794b4869c89c59d93cd593b413ab921abd06200
EvgenyKuchko <kuchkozhenia@gmail.com>
2022-05-12 14:42:51 +0300

    add query method to PictureRepository

--- src/main/java/com/project/marketplace/repository/PictureRepository.java
+++ src/main/java/com/project/marketplace/repository/PictureRepository.java
@@ -2,6 +2,12 @@ package com.project.marketplace.repository;
 
 import com.project.marketplace.model.Picture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PictureRepository extends JpaRepository<Picture, Long> {
+    @Modifying
+    @Query("update Picture p set p.price = :price where p.id = :id")
+    void changePrice(@Param("price") Float price, @Param("id") long id);
 }
