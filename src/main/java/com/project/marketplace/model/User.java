79ed27281077a6c3815af77cca2d1367df7b84de
EvgenyKuchko <kuchkozhenia@gmail.com>
2022-05-14 13:27:08 +0300

    created class Role, implements GrantedAuth implemented UserDetails to User and override methods for auth added to WebSecurityConfig class override methods to auth implemented UserDetailsService by UserService and override loadUserByUsername method

--- src/main/java/com/project/marketplace/model/User.java
+++ src/main/java/com/project/marketplace/model/User.java
@@ -3,15 +3,18 @@ package com.project.marketplace.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -30,4 +33,38 @@ public class User {
     private Set<Offer> sentOffers;
     @OneToMany(mappedBy = "salesman")
     private Set<Offer> receivedOffers;
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
