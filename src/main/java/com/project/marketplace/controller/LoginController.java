b231936e83b28ce0f2d60ccca743e3b2c5ce9487
EvgenyKuchko <kuchkozhenia@gmail.com>
2022-05-16 13:28:48 +0300

    added processing for invalid data when user auth

--- src/main/java/com/project/marketplace/controller/LoginController.java
+++ src/main/java/com/project/marketplace/controller/LoginController.java
@@ -3,8 +3,10 @@ package com.project.marketplace.controller;
 import com.project.marketplace.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
@@ -14,7 +16,10 @@ public class LoginController {
     private UserService userService;
 
     @GetMapping()
-    public String showLoginForm() {
+    public String showLoginForm(@RequestParam(value = "error", required = false) String error, Model model) {
+        if(error != null) {
+            model.addAttribute("error", "Invalid email or password!");
+        }
         return "login";
     }
 }
