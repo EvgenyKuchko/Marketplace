1eb2117483fb687411f17ad5ad08d24adbe86497
EvgenyKuchko <kuchkozhenia@gmail.com>
2022-06-05 15:25:59 +0300

    add user to DB

---
+++ src/main/resources/db/migration/V2__Init_Rows.sql
@@ -0,0 +1,5 @@
+INSERT INTO user
+VALUES (1, 'user@gmail.com', 'user', '$2a$08$DbGLBKBDeQeZd9wsLAln0uGgU1MUtoOxqdNRTnsXps5fOup/3wneK', 'some words about user', 100.0,'default-profile-picture.png');
+
+INSERT INTO user_role
+VALUES (1, 'USER');
